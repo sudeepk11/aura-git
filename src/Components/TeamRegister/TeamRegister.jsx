dcd2de3fcc59cc8e5c2d49a6bf2c371e54fe15ba
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2024-03-03 22:54:00 +0530

    fix: incorrect registration fee displayed after registering for an event

--- src/Components/TeamRegister/TeamRegister.jsx
+++ src/Components/TeamRegister/TeamRegister.jsx
@@ -8,6 +8,19 @@ import errors from "../../Utils/error.codes.json";
 import { redirect } from "react-router-dom";
 import payqr from "../../Assets/qr.png";
 
+function getRegistrationFeesForTeam(team) {
+  const teamSize = 1 + (team?.team_members?.length ?? 0);
+
+  if (teamSize === 1)
+    return 99;
+  if (teamSize === 2)
+    return 199;
+  if (teamSize <= 4)
+    return 299;
+
+  return 399;
+}
+
 const TeamRegister = (props) => {
   const [team, setTeam] = useState([]);
   const [name, setName] = useState("");
@@ -262,7 +275,7 @@ const TeamRegister = (props) => {
                   <p className="my-4 text-blue-600 text-md">
                     Please open your preferred payment app and please pay{" "}
                     <strong className="highlight">
-                      ₹{teamSize === 1 ? "50" : "250"}
+                      ₹ {getRegistrationFeesForTeam(props.team)}
                     </strong>{" "}
                     by scanning the QR code displayed above.
                   </p>
