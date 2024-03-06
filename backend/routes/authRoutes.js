5a30e0ab0228f2441b5db441faa509a2d67f55ac
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2024-03-06 20:27:38 +0530

    feat: setup rate limiting on auth signup route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,21 @@
 // Imports
 const { Router } = require("express");
+const { rateLimit } = require("express-rate-limit");
+const stdResponse = require("../models/standard.response.model");
 const { checkUser } = require("../middleware/authMiddleware");
 const { complete } = require("../controllers/controllers");
 const authController = require("../controllers/authController");
 
 // Constants
 const router = Router();
+const oneRequestPer2MinutesRateLimiter = rateLimit({
+  limit: 1,
+  windowMs: 2 * 60 * 1000, // 2 mins
+  message: stdResponse("Rate limited"),
+});
 
 // Body
-router.post("/signup", authController.signup_post, complete);
+router.post("/signup", oneRequestPer2MinutesRateLimiter, authController.signup_post, complete);
 router.post("/login", authController.login_post, complete);
 router.get("/logout", authController.logout_get, complete);
 
