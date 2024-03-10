42fc55cd0c81afe808e5992c37b288682619d2c6
KirPot <kiran@dev.com>
2024-03-10 15:09:51 +0530

    fixed schedule img

--- src/Utils/axios.config.js
+++ src/Utils/axios.config.js
@@ -4,7 +4,6 @@ const api = axios.create({
   // baseURL: "http://localhost:3001",
   // baseURL: "http://3.110.70.229:3001",
   baseURL: "https://aura-git-frontend.onrender.com",
-  // baseURL: "https://aura-git.onrender.com",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
