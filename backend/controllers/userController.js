cad015b79cd5db16c596e42b15c2bd3373c6f222
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2024-03-10 14:23:20 +0530

    feat: users api to get a user by aura id

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,136 +7,138 @@ const { errorHandler, quoteRegExp } = require("../utils/utils");
 
 // Body
 async function userGetController(req, res, next) {
-	try {
-		const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-		const user = await User.findById(id, "-password");
-		if (!user) return res.status(404).send(Response(errors[404].userNotFound));
+    const user = await User.findById(id, "-password");
+    if (!user) return res.status(404).send(Response(errors[404].userNotFound));
 
-		if (!res.locals.data)
-			res.locals.data = {};
-		res.locals.data.user = user;
-	} catch (error) {
-		const { status, message } = errorHandler(error);
-		return res.status(status).send(Response(message));
-	}
+    if (!res.locals.data) res.locals.data = {};
+    res.locals.data.user = user;
+  } catch (error) {
+    const { status, message } = errorHandler(error);
+    return res.status(status).send(Response(message));
+  }
 
-	return next();
+  return next();
+}
+
+async function userGetByAuraIdController(req, res, next) {
+  try {
+    const { auraId } = req.params;
+
+    const user = await User.findOne({ aura_id: auraId }, "-password");
+    if (!user) return res.status(404).send(Response(errors[404].userNotFound));
+
+    if (!res.locals.data) res.locals.data = {};
+    res.locals.data.user = user;
+  } catch (error) {
+    const { status, message } = errorHandler(error);
+    return res.status(status).send(Response(message));
+  }
+
+  return next();
 }
 
 async function userSearchController(req, res, next) {
-	try {
-		let {
-			aura_id = undefined,
-			email = undefined,
-			college = undefined,
-			name = undefined,
-			usn = undefined,
-			phone = undefined,
-			email_verified = undefined,
-			pageSize = queryConfig["search.pagination"]["page.size"],
-			paginationTs = Date.now(),
-		} = req.query;
-		pageSize = parseInt(pageSize, 10);
-		paginationTs = parseInt(paginationTs, 10);
+  try {
+    let {
+      aura_id = undefined,
+      email = undefined,
+      college = undefined,
+      name = undefined,
+      usn = undefined,
+      phone = undefined,
+      email_verified = undefined,
+      pageSize = queryConfig["search.pagination"]["page.size"],
+      paginationTs = Date.now(),
+    } = req.query;
+    pageSize = parseInt(pageSize, 10);
+    paginationTs = parseInt(paginationTs, 10);
 
-		if (!aura_id
-			&& !email
-			&& !college
-			&& !name
-			&& !usn
-			&& !phone
-			&& (email_verified === undefined || !/^(true|false)$/i.test(email_verified)))
-			return res.status(400).send(Response(errors[400].searchQueryRequired));
+    if (
+      !aura_id &&
+      !email &&
+      !college &&
+      !name &&
+      !usn &&
+      !phone &&
+      (email_verified === undefined || !/^(true|false)$/i.test(email_verified))
+    )
+      return res.status(400).send(Response(errors[400].searchQueryRequired));
 
-		if (aura_id)
-			aura_id = quoteRegExp(aura_id);
-		if (email)
-			email = quoteRegExp(email);
-		if (college)
-			college = quoteRegExp(college);
-		if (name)
-			name = quoteRegExp(name);
-		if (usn)
-			usn = quoteRegExp(usn);
-		if (phone)
-			phone = quoteRegExp(phone);
-		if (typeof email_verified === "string")
-			email_verified = email_verified.toLowerCase() === "true";
+    if (aura_id) aura_id = quoteRegExp(aura_id);
+    if (email) email = quoteRegExp(email);
+    if (college) college = quoteRegExp(college);
+    if (name) name = quoteRegExp(name);
+    if (usn) usn = quoteRegExp(usn);
+    if (phone) phone = quoteRegExp(phone);
+    if (typeof email_verified === "string") email_verified = email_verified.toLowerCase() === "true";
 
-		const query = {};
-		if (aura_id)
-			query.aura_id = { $regex: queryConfig["search.options"].aura_id.replace("{aura_id}", aura_id), $options: "i" };
-		if (email)
-			query.email = { $regex: queryConfig["search.options"].email.replace("{email}", email), $options: "i" };
-		if (college)
-			query.college = { $regex: queryConfig["search.options"].college.replace("{college}", college), $options: "i" };
-		if (name)
-			query.name = { $regex: queryConfig["search.options"].name.replace("{name}", name), $options: "i" };
-		if (usn)
-			query.usn = { $regex: queryConfig["search.options"].usn.replace("{usn}", usn), $options: "i" };
-		if (phone)
-			query.phone = { $regex: queryConfig["search.options"].phone.replace("{phone}", phone), $options: "i" };
-		if (typeof email_verified === "boolean")
-			query.email_verified = email_verified;
+    const query = {};
+    if (aura_id) query.aura_id = { $regex: queryConfig["search.options"].aura_id.replace("{aura_id}", aura_id), $options: "i" };
+    if (email) query.email = { $regex: queryConfig["search.options"].email.replace("{email}", email), $options: "i" };
+    if (college) query.college = { $regex: queryConfig["search.options"].college.replace("{college}", college), $options: "i" };
+    if (name) query.name = { $regex: queryConfig["search.options"].name.replace("{name}", name), $options: "i" };
+    if (usn) query.usn = { $regex: queryConfig["search.options"].usn.replace("{usn}", usn), $options: "i" };
+    if (phone) query.phone = { $regex: queryConfig["search.options"].phone.replace("{phone}", phone), $options: "i" };
+    if (typeof email_verified === "boolean") query.email_verified = email_verified;
 
-		if (typeof pageSize === "string")
-			pageSize = parseInt(pageSize, 10);
-		if (pageSize <= 0 || pageSize > queryConfig["search.pagination"]["page.max.size"])
-			pageSize = queryConfig["search.pagination"]["page.size"];
+    if (typeof pageSize === "string") pageSize = parseInt(pageSize, 10);
+    if (pageSize <= 0 || pageSize > queryConfig["search.pagination"]["page.max.size"])
+      pageSize = queryConfig["search.pagination"]["page.size"];
 
-		const users = await User.find({
-			...query,
-			"_profile_information.account_creation_timestamp": { $lte: paginationTs },
-		}, "-password")
-			.sort({ "_profile_information.account_creation_timestamp": -1 })
-			.limit(pageSize + 1);
+    const users = await User.find(
+      {
+        ...query,
+        "_profile_information.account_creation_timestamp": { $lte: paginationTs },
+      },
+      "-password"
+    )
+      .sort({ "_profile_information.account_creation_timestamp": -1 })
+      .limit(pageSize + 1);
 
-		if (!res.locals.data)
-			res.locals.data = {};
-		res.locals.data.pageSize = pageSize;
-		res.locals.data.resultsSize = (users.length === pageSize + 1 ? pageSize : users.length);
-		res.locals.data.paginationTs = (users.length - 1 === pageSize ? users[users.length - 1]._profile_information.account_creation_timestamp : null);
-		res.locals.data.results = users.slice(0, pageSize).filter(value => !!value);
-	} catch (error) {
-		const { status, message } = errorHandler(error);
-		return res.status(status).send(Response(message));
-	}
+    if (!res.locals.data) res.locals.data = {};
+    res.locals.data.pageSize = pageSize;
+    res.locals.data.resultsSize = users.length === pageSize + 1 ? pageSize : users.length;
+    res.locals.data.paginationTs =
+      users.length - 1 === pageSize ? users[users.length - 1]._profile_information.account_creation_timestamp : null;
+    res.locals.data.results = users.slice(0, pageSize).filter((value) => !!value);
+  } catch (error) {
+    const { status, message } = errorHandler(error);
+    return res.status(status).send(Response(message));
+  }
 
-	return next();
+  return next();
 }
 
 async function userUpdateController(req, res, next) {
-	if (!res.locals.user)
-		return res.status(401).send(Response(errors[401].authRequired));
+  if (!res.locals.user) return res.status(401).send(Response(errors[401].authRequired));
 
-	try {
-		const { body } = req;
+  try {
+    const { body } = req;
 
-		const { name = undefined, usn = undefined, college = undefined, phone = undefined } = body;
+    const { name = undefined, usn = undefined, college = undefined, phone = undefined } = body;
 
-		const user = res.locals.user;
-		if (name !== undefined)
-			user.name = name;
-		if (usn !== undefined)
-			user.usn = usn;
-		if (college !== undefined)
-			user.college = college;
-		if (phone !== undefined)
-			user.phone = phone;
+    const user = res.locals.user;
+    if (name !== undefined) user.name = name;
+    if (usn !== undefined) user.usn = usn;
+    if (college !== undefined) user.college = college;
+    if (phone !== undefined) user.phone = phone;
 
-		await res.locals.user.save();
-		await res.locals.refreshProfile();
-	} catch (error) {
-		const { status, message } = errorHandler(error);
-		return res.status(status).send(Response(message));
-	}
+    await res.locals.user.save();
+    await res.locals.refreshProfile();
+  } catch (error) {
+    const { status, message } = errorHandler(error);
+    return res.status(status).send(Response(message));
+  }
 
-	return next();
+  return next();
 }
 
 module.exports = {
-	userGetController,
-	userSearchController,
-	userUpdateController,
+  userGetController,
+  userGetByAuraIdController,
+  userSearchController,
+  userUpdateController,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,15 @@
 // Imports
 const express = require("express");
+const { param } = require("express-validator");
+const errors = require("../configs/error.codes.json");
+const { expressValidationErrorHandler } = require("../middleware/validationErrorHandler");
 const { requireVerifiedAuth } = require("../middleware/authMiddleware");
 const { complete } = require("../controllers/controllers");
 const {
-	userGetController,
-	userSearchController,
-	userUpdateController,
+  userGetController,
+  userGetByAuraIdController,
+  userSearchController,
+  userUpdateController,
 } = require("../controllers/userController");
 
 // Constants
@@ -14,6 +18,17 @@ const Router = express.Router();
 // Body
 Router.get("/search", userSearchController, complete);
 Router.get("/:id", userGetController, complete);
+Router.get(
+  "/aura-id/:auraId",
+  param("auraId")
+    .exists()
+    .withMessage(errors[400].auraIdRequired)
+    .matches(/^AURA24-[A-Z]{3}-\d{5}$/)
+    .withMessage(errors[400].invalidAuraId),
+  expressValidationErrorHandler,
+  userGetByAuraIdController,
+  complete
+);
 
 Router.patch("/", requireVerifiedAuth, userUpdateController, complete);
 
